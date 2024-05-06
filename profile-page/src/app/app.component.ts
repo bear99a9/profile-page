3295636d3a15dea4ae0ccc8794f485b1ce2661f1
Sean <sean.edwards@teachingpersonnel.com>
2024-05-06 15:49:44 +0100

    Added main component

--- profile-page/src/app/app.component.ts
+++ profile-page/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'profile-page';
 }
