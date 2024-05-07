4792686e66756decb245eb6f68c885ee99af73cc
Sean <sean.edwards@teachingpersonnel.com>
2024-05-07 09:55:46 +0100

    Started to add an about section

--- profile-page/src/app/main/main.component.ts
+++ profile-page/src/app/main/main.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { LinksComponent } from './links/links.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [LinksComponent, ExperienceComponent, AboutComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
